6a2d36953f4249aca1942aba6edaeabefbc85fbd
ssarthak598 <sarthak.sethi@hotmail.com>
2017-07-28 10:25:09 +0530

    new

---
+++ BLE_Interface.ino
@@ -0,0 +1,140 @@
+
+#include "FPS_GT511C3.h"
+#include "SoftwareSerial.h"
+#include <Wire.h>
+#include <ADXL345.h>
+
+
+ADXL345 adxl;
+// Hardware setup - FPS connected to:
+//    digital pin 4(arduino rx, fps tx)
+//    digital pin 5(arduino tx - 560ohm resistor fps tx - 1000ohm resistor - ground)
+//    this brings the 5v tx line down to about 3.2v so we dont fry our fps
+
+FPS_GT511C3 fps(5, 4);
+SoftwareSerial mySerial(10, 11);
+SoftwareSerial BTSerial(9, 8);
+
+void setup()
+{
+  Serial.begin(9600);
+  adxl.powerOn();
+
+  //set activity/ inactivity thresholds (0-255)
+  adxl.setActivityThreshold(75); //62.5mg per increment
+  adxl.setInactivityThreshold(75); //62.5mg per increment
+  adxl.setTimeInactivity(10); // how many seconds of no activity is inactive?
+ 
+  //look of activity movement on this axes - 1 == on; 0 == off 
+  adxl.setActivityX(1);
+  adxl.setActivityY(1);
+  adxl.setActivityZ(1);
+ 
+  //look of inactivity movement on this axes - 1 == on; 0 == off
+  adxl.setInactivityX(1);
+  adxl.setInactivityY(1);
+  adxl.setInactivityZ(1);
+ 
+  //look of tap movement on this axes - 1 == on; 0 == off
+  adxl.setTapDetectionOnX(0);
+  adxl.setTapDetectionOnY(0);
+  adxl.setTapDetectionOnZ(1);
+ 
+  //set values for what is a tap, and what is a double tap (0-255)
+  adxl.setTapThreshold(50); //62.5mg per increment
+  adxl.setTapDuration(15); //625us per increment
+  adxl.setDoubleTapLatency(80); //1.25ms per increment
+  adxl.setDoubleTapWindow(200); //1.25ms per increment
+ 
+  //set values for what is considered freefall (0-255)
+  adxl.setFreeFallThreshold(7); //(5 - 9) recommended - 62.5mg per increment
+  adxl.setFreeFallDuration(45); //(20 - 70) recommended - 5ms per increment
+ 
+  //setting all interrupts to take place on int pin 1
+  //I had issues with int pin 2, was unable to reset it
+  adxl.setInterruptMapping( ADXL345_INT_SINGLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_DOUBLE_TAP_BIT,   ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_FREE_FALL_BIT,    ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_ACTIVITY_BIT,     ADXL345_INT1_PIN );
+  adxl.setInterruptMapping( ADXL345_INT_INACTIVITY_BIT,   ADXL345_INT1_PIN );
+ 
+  //register interrupt actions - 1 == on; 0 == off  
+  adxl.setInterrupt( ADXL345_INT_SINGLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_DOUBLE_TAP_BIT, 1);
+  adxl.setInterrupt( ADXL345_INT_FREE_FALL_BIT,  1);
+  adxl.setInterrupt( ADXL345_INT_ACTIVITY_BIT,   1);
+  adxl.setInterrupt( ADXL345_INT_INACTIVITY_BIT, 1);
+  
+  mySerial.begin(9600);
+  mySerial.print('i');
+  BTSerial.begin(9600);
+  BTSerial.print('i');
+  delay(100);
+  fps.Open();
+  fps.SetLED(true);
+  
+}
+
+void loop()
+{
+
+  // Identify fingerprint test
+  if (fps.IsPressFinger())
+  {
+    fps.CaptureFinger(false);
+    int id = fps.Identify1_N();
+    if (id <200)
+    {
+      Serial.print("Verified ID:");
+      Serial.println(id);
+      mySerial.print(id);
+      BTSerial.print(id);
+    }
+    else
+    {
+      Serial.println("Finger not found");
+      
+    }
+  }
+  else
+  {
+    Serial.println("Please press finger");
+    int x,y,z;  
+  adxl.readXYZ(&x, &y, &z); //read the accelerometer values and store them in variables  x,y,z
+  // Output x,y,z values 
+  Serial.print("values of X , Y , Z: ");
+  Serial.print(x);
+  Serial.print(" , ");
+  Serial.print(y);
+  Serial.print(" , ");
+  Serial.println(z);
+   if(200 < x)
+  {
+    Serial.println(6);
+    mySerial.println(6);
+  }
+  if(x < -200)
+  {
+    Serial.println(7);
+    mySerial.println(7);
+  }
+  
+  if(200 < z)
+  {
+    Serial.println(8);
+    mySerial.println(8);
+  }
+  if(z < -200)
+  {
+    Serial.println(9);
+    mySerial.println(9);
+  }
+  
+  delay(500);
+   // mySerial.println('i');
+  }
+
+ // if(analogRead(
+
+  delay(100);
+}
